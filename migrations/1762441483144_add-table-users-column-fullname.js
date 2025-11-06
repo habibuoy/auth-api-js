6c98d760cb794a7510021652a1546395f5b99c37
habibuoy <habibiemuhammad90@gmail.com>
2025-11-06 22:21:09 +0700

    Chore: add migrations for adding column fullname in table users

---
+++ migrations/1762441483144_add-table-users-column-fullname.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addColumns('users', {
+    fullname: {
+      type: 'TEXT',
+      notnull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropColumns('users', ['fullname']);
+};
